fae9cffb013c66f2d43149ae373b9c4ff4419703
Chris Hatcher <hatcherchris20@gmail.com>
2022-01-20 05:26:18 +0000

    adding client and play

--- play.js
+++ play.js
@@ -5,28 +5,16 @@
 
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({                 // You used Node's net library (specifically, the createConnection function) to create an object named conn in the code above.
-    host: '165.227.47.243',   // IP address here,        
-    port: '50541',            // PORT number here,
-  });
 
-  console.log("connected to server");
-  
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+// Moved connect function to client.js
+// Need to export it from client.js to use it here in play.js
 
-  conn.on("data", (data) => {
-    console.log(data.toString());
-    console.log("Flames rule");
-    conn.end();
-  });
 
-  return conn;
-  // The conn object that Node returned to you represents the connection that you have with the server.
-  // The conn object is full of useful methods and properties that can now be used to interact with the server!
-};
+const {connect} = require("./client.js");
+
+
+
+
 
 console.log("Connecting ...");
 const connection = connect();
